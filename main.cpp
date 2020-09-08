804038c2803d02de0e25692156217458cdb96654
xdie44 <diego.capoccitti@gmail.com>
2020-09-08 10:51:05 +0000

    part 1/1 finished

--- main.cpp
+++ main.cpp
@@ -52,85 +52,85 @@ arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: water
+//  action 1: the water flows
+water.flow();
+//  action 2: the water wets things
+water.wet();
+//  action 3: the water quenches thirst 
+water.quenche();
 //  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: air
+//  action 1: the air creates pressure 
+air.createPressure();
+//  action 2: the air fills empty spaces
+air.fillSpaces();
+//  action 3: the air moves things 
+air.moveThings()();
+//  3) 
+//  Noun: fire 
+//  action 1: fire burns
+fire.burn();
+//  action 2: fire warms up
+fire.warmUp();
+//  action 3: fire melts 
+fire.melt();
+//  4) 
+//  Noun: ice 
+//  action 1: ice freezes
+ice.freeze();
+//  action 2: ice cools
+ice.cool();
+//  action 3: ice expands
+ice.expand(); 
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: wind
+//  action 1: the wind blows
+wind.blow();
+//  action 2: the wind moves the clouds
+wind.movesClouds();
+//  action 3: the wind makes noise
+wind.makeNoise();
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: energy
+//  action 1: the energy flows
+energy.flow();
+//  action 2: the energy makes things move
+energy.makeMove();
+//  action 3: the energy provides heat
+energy.provideHeat();
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: tree
+//  action 1: the tree grow
+tree.grow();
+//  action 2: the tree produce fruits
+tree.provideFruit();
+//  action 3:the tree produces oxygen 
+tree.produceOxygen();
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: sun
+//  action 1: the sun illuminates
+sun.illuminate();
+//  action 2: the sun rises
+sun.rise();
+//  action 3: the sun sets
+sun.sets();
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: bird
+//  action 1: the bird flies 
+bird.fly();
+//  action 2: the bird eats
+bird.eat();
+//  action 3: the bird tweets
+bird.tweet();
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: dog
+//  action 1: the dog run
+dog.run();
+//  action 2: the dog barks
+dog.bark();
+//  action 3: the dog bites
+dog.bite();
 
 
 #endif
